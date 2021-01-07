82adaf8e3718b9aaff70e9b0f2458d7c1a7eeb89
TRD <tilakgo@gmail.com>
2021-01-07 12:50:56 +0530

    testing git

--- app/src/main/java/com/example/multi_cameraapp/MainActivity.java
+++ app/src/main/java/com/example/multi_cameraapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.multi_cameraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String j;
+        Toast.makeText(this,"hello vedant",Toast.LENGTH_SHORT).show();
     }
 }
