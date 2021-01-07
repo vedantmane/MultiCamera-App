2cde4848bf66c49f7f1ebfaf59669b880e960ef2
TRD <tilakgo@gmail.com>
2021-01-07 13:02:06 +0530

    deleted file

--- app/src/main/java/com/example/multi_cameraapp/MainActivity.java
+++ app/src/main/java/com/example/multi_cameraapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.multi_cameraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/multi_cameraapp/temp.java
+++
@@ -1,4 +0,0 @@
-package com.example.multi_cameraapp;
-
-public class temp {
-}
